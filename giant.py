60ff1b6c4a1dadbb64085b2c2cb27c1db8dbc9c4
gestalt8003 <cmcummings2003@gmail.com>
2018-11-17 19:33:05 -0600

    update

--- giant.py
+++ giant.py
@@ -34,11 +34,47 @@ def sigmoid(x):
 def sigmoid_p(x):
 	return sigmoid(x) * (1-sigmoid(x))
 
-T = np.linspace(-5, 5, 10)
-Y = sigmoid(T)
+# T = np.linspace(-5, 5, 10)
+# Y = sigmoid(T)
+
+learning_rate = 0.5
+costs = []
 
 # Training loop
-for i in range(1000):
-	ri = np.random.randint(len(data)) # Random index 
+for i in range(50000):
+	ri = np.random.randint(len(data)) # Random index
 	point = data[ri]
+
+	z = point[0] * w1 + point[1] * w2 + b
+	pred = sigmoid(z) # Activation function. pred=prediction
+
+	target = point[2]
+	cost = np.square(pred - target)
+
+	dcost_pred = 2 * (pred - target)
+	dpred_dz = sigmoid_p(z)
+	dz_dw1 = point[0]
+	dz_dw2 = point[1]
+	dz_db = 1
+	dcost_dz = dcost_pred * dpred_dz
+
+	dcost_dw1 = dcost_dz * dz_dw1
+	dcost_dw2 = dcost_dz * dz_dw2
+	dcost_db = dcost_dz * dz_db
+
+	w1 = w1 - learning_rate * dcost_dw1
+	w2 = w2 - learning_rate * dcost_dw2
+	b = b - learning_rate * dcost_db
+
+# Predictions
+for i in range(len(data)):
+	point = data[i]
 	print(point)
+	z = point[0] * w1 + point[1] * w2 + b
+	pred = sigmoid(z)
+	print("pred: {}".format(pred))
+
+
+z = mystery_flower[0] * w1 + mystery_flower[1] * w2 + b
+pred = sigmoid(z)
+print("pred: {}".format(pred))
